c8832faa671d459a0b04c582554e05e4189c6115
Kazuhisa Hara <kazuhisya@gmail.com>
2015-12-30 20:50:53 +0900

    Fixed date to unnecessary location is displayed

--- lib/redmine_default_due_date.rb
+++ lib/redmine_default_due_date.rb
@@ -4,6 +4,7 @@ module Redmine
     class DefaultDueDateListener < Redmine::Hook::ViewListener
       def view_issues_new_top(context)
         context[:issue].due_date ||= Date.today + 7
+        nil
       end
     end
   end
